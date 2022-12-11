46bcf2e7a634f3c796abd16ea6ed7527af2a5656
Maciej Gojlik <60773657+Pompeiro@users.noreply.github.com>
2022-12-11 14:05:40 +0100

    Remove debug screenshots code (#5)

--- pages/base.py
+++ pages/base.py
@@ -12,12 +12,3 @@ class BasePage:
 
     def navigate(self) -> None:
         self.page.goto(self.combine_url())
-
-    def make_debug_screenshot(self, node_id:str, screenshot_name:str, debug_mode:bool=False) -> None:
-        """
-        test_node_id: 'tests/testarena/test_password_recovery.py::test_is_redirected_to_login_page_by_clicking_login[chromium]'
-        test_name: test_is_redirected_to_login_page_by_clicking_login[chromium]
-        """
-        test_name = node_id[node_id.find("::") + 2:]
-        if debug_mode:
-            self.page.screenshot(path="screenshots/" + test_name + "/" + screenshot_name + ".jpg",full_page=True)
